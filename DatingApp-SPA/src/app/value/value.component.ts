81e5364db2d1257e833dc44617eee60f5eca4f72
Roman Kiss <romiki44@hotmail.com>
2019-04-04 19:31:32 +0200

    Server Authentication Done

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -16,11 +16,11 @@ export class ValueComponent implements OnInit {
   }
 
   getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(response => { 
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
         this.values = response;
-        console.log(this.values); 
-      }, error => { 
-        console.log(error); 
+        console.log(this.values);
+      }, error => {
+        console.log(error);
       }
     );
   }
