e3d5da0a55430dabb60f359969c5a041ffb1988a
Roman Kiss <romiki44@hotmail.com>
2019-04-07 16:01:06 +0200

    Adding 3rg party components

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,27 +10,26 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
-    //console.log(this.model);
+    // console.log(this.model);
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+      this.alertify.success('Logged in successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   logedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.logedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logedd out');
+    this.alertify.message('logedd out');
   }
 }
