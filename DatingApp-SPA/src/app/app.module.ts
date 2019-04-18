56f61aa81915029b866f170ae349a19ea91c577d
Roman Kiss <romiki44@hotmail.com>
2019-04-18 21:02:48 +0200

    Adding ordering, filtering, sorting

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule} from 'ngx-gallery';
@@ -63,6 +63,8 @@ export function tokenGetter() {
       TabsModule.forRoot(),
       BsDropdownModule.forRoot(),
       BsDatepickerModule.forRoot(),
+      PaginationModule.forRoot(),
+      ButtonsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
          config: {
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,7 +13,7 @@ namespace DatingApp.API.Data
         private readonly DataContext _context;
         public DatingRepository(DataContext context)
         {
-            _context=context;
+            _context = context;
         }
         public void Add<T>(T entity) where T : class
         {
@@ -25,28 +27,52 @@ namespace DatingApp.API.Data
 
         public async Task<Photo> GetMainPhotoForUser(int userId)
         {
-            return await _context.Photos.Where(u=>u.UserId==userId)
-                                .FirstOrDefaultAsync(p=>p.IsMain);
+            return await _context.Photos.Where(u => u.UserId == userId)
+                                .FirstOrDefaultAsync(p => p.IsMain);
         }
 
         public async Task<Photo> GetPhoto(int id)
         {
-            return await _context.Photos.FirstOrDefaultAsync(p=>p.Id==id);
+            return await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<User> GetUser(int id)
         {
-            return await _context.Users.Include(p=>p.Photos).FirstOrDefaultAsync(u=>u.Id==id);
+            return await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            return await _context.Users.Include(p=>p.Photos).ToListAsync();
+            var users = _context.Users.Include(p => p.Photos)
+                            .OrderByDescending(u=>u.LastActive).AsQueryable();
+
+            users = users.Where(u => u.Id != userParams.UserId);
+            users = users.Where(u => u.Gender == userParams.Gender);
+
+            if(userParams.MinAge!=18 || userParams.MaxAge!=99) {
+                var minDob=DateTime.Today.AddYears(-userParams.MaxAge-1);
+                var maxDob=DateTime.Today.AddYears(-userParams.MinAge);
+
+                users=users.Where(u=>u.DateOfBirth>=minDob && u.DateOfBirth<=maxDob);
+            }
+            
+            if(!string.IsNullOrEmpty(userParams.OrderBy)) {
+                switch(userParams.OrderBy) {
+                    case "created":
+                        users=users.OrderByDescending(u=>u.Created);
+                        break;
+                    default:
+                        users=users.OrderByDescending(u=>u.LastActive);
+                        break;
+                }
+            }
+
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
         {
-            return await _context.SaveChangesAsync()>0;
+            return await _context.SaveChangesAsync() > 0;
         }
     }
 }
